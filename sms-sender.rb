2beed97077dae4b1dfb04b8831516a4a5bcf6260
Stephen Roth <stephenroth.sr@gmail.com>
2014-05-02 16:45:13 -0400

    added method to sms-sender

--- sms-sender.rb
+++ sms-sender.rb
@@ -21,25 +21,20 @@ class SMSsender
 
 	end
 
-	# def self.send_sms_to_restaurant(output_from_ujerdatabase)
+	def self.send_sms_to_user(output_from_ujerdatabase)
 
-	# 	account_sid = "ACebfdf1512a26dd76959e738d39835611"
-	# 	auth_token = "9d8da5e6ffdb46e7ac567638c82f3fa5"
-	# 	client = Twilio::REST::Client.new account_sid, auth_token 
-	# 	from = "+19149025937" # Your Twilio number
-		 
-	# 	restaurant = {
-	# 	"+19145523727" => "Stephen",
-	# 	}
-	# 	# restaurants.each do |key, value|
-	# 	  client.account.messages.create(
-	# 	    :from => from,
-	# 	    :to => key,
-	# 	    :body => order_info
-	# 	  ) 
-	# 	end
-	# end
+		account_sid = "ACebfdf1512a26dd76959e738d39835611"
+		auth_token = "9d8da5e6ffdb46e7ac567638c82f3fa5"
+		client = Twilio::REST::Client.new account_sid, auth_token 
+		from = "+19149025937" # Your Twilio number
+
+	  client.account.messages.create(
+	    :from => from,
+	    :to => "+19145523727",
+	    :body => order_info
+	  ) 
+	end
 
 end
 
-SMSsender.send_sms_to_restaurant
+# SMSsender.send_sms_to_restaurant
